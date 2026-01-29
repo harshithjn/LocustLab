77e8632adb6f7f64850aeb7846b815a56abd8e24
pseudo0244 <142434147+pseudo0244@users.noreply.github.com>
2026-01-29 11:41:24 +0530

    PES2UG23CS216

---
+++ locust/events_locustfile.py
@@ -0,0 +1,9 @@
+from locust import HttpUser, task, between
+
+class EventsUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def view_events(self):
+        self.client.get("/events?user=locust_user")
+        
